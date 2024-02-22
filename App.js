fc7e2496bd56a625afbffd351f3a03b42d4b3e81
Eren AVAR <eren.avar@hotmail.com>
2024-02-22 11:51:07 +0300

    Created Colors Folder & Set Up Bg Color of Main Page

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { colors } from "./src/colors";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
   },
+  text: { color: colors.white },
 });
