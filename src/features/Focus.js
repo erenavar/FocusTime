0dd03412db5e16f168d5b4ec70a165dbe5a44ae9
Eren AVAR <eren.avar@hotmail.com>
2024-03-05 19:53:21 +0300

    Stored a Suvject

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { TextInput } from "react-native-paper";
 import { colors } from "../utils/colors";
 
-export const Focus = () => (
-  <View style={styles.container}>
-    <TextInput label="Focus Item" />
-  </View>
-);
+export const Focus = () => {
+  useState[(subject, setSubject)] = useState();
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={setSubject}
+          label="What would like to focus on?"
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.darkBlue,
   },
-  text: { color: colors.white },
+  inputContainer: {
+    flex: 0.5,
+    padding: 25,
+    justifyContent: "top",
+  },
 });
