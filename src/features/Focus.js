bcb328bab9e3fab00ce661c1acfbdc1fdf556495
Eren AVAR <eren.avar@hotmail.com>
2024-03-24 19:54:17 +0100

    Added Button

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -11,10 +11,13 @@ export const Focus = () => {
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
+          style={styles.textInput}
           onChangeText={setSubject}
           label="What would like to focus on?"
         />
-        <RoundedButton title="+" size={50} />
+        <View style={styles.button}>
+          <RoundedButton title="+" size={50} />
+        </View>
       </View>
     </View>
   );
@@ -23,6 +26,12 @@ export const Focus = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  button: { justifyContent: "center" },
+  textInput: {
+    flex: 1,
+    paddingRight: 10,
+    marginRight: 10,
   },
   inputContainer: {
     padding: 25,
