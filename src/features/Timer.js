8b7964d57c5d819bf6609013ded930c0b4452ad7
Eren AVAR <eren.avar@hotmail.com>
2024-05-06 10:39:53 +0200

    add start button and isStart state

--- src/features/Timer.js
+++ src/features/Timer.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
+import { Countdown } from "../components/Countdown";
+import { RoundedButton } from "../components/RoundedButton";
 
 export const Timer = ({ focusSubject }) => {
+  const [isStart, setIsStart] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Focus Subject {focusSubject}</Text>
+      <View style={styles.countdown}>
+        <Countdown isPaused onProgress={() => {}} onEnd={() => {}} />
+      </View>
+      <View style={styles.buttonWrapper}>
+        <RoundedButton title="start" />
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  countdown: {
+    flex: 0.5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonWrapper: {
+    flex: 0.3,
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
